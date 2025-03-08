52e020445efae7792fec12055c0d3313dc6de6a4
writetochetanpandey <writetochetanpandey@gmail.com>
2025-03-09 02:43:41 +0700

    Optimizations at Sun 03-09-20252-43-40 "

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  serverExternalPackages: ["mongodb"],
+  experimental: {
+    turbo: {}
+  },
+  serverExternalPackages: ["mongodb"]
 };
 
 export default nextConfig;
